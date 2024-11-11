eff203006f0eec99fb4874f9df0ba1fa3c7855b0
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-10 23:52:01 -0500

    First test

--- YarbisIA/mudandoIA/tests.py
+++ YarbisIA/mudandoIA/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class IndexViewTest(TestCase):
+    def test_index_view_status_code(self):
+        # Utiliza reverse para generar la URL para la vista 'index'
+        response = self.client.get(reverse('index'))
+        # Comprueba que la vista devuelve un código de estado 200
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_view_uses_correct_template(self):
+        response = self.client.get(reverse('index'))
+        # Comprueba que la vista utiliza el template correcto
+        self.assertTemplateUsed(response, 'mudandoIA/index.html')
