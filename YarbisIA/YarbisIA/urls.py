5c3c7c53a211ae752707296e0a180a8e16ca6294
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-10 23:00:56 -0500

    CHEGES SMALL0

--- YarbisIA/YarbisIA/urls.py
+++ YarbisIA/YarbisIA/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-import urls
+from django.urls import path, include
+import urls, mudandoIA
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', admin.mudandoIA.urls )
+     path('', include('mudandoIA.urls')),
 ]
