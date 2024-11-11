b469c766744de8cc4003560d24a0f089f2d9195d
Contreras Perales Rodrigo <levicom134@gmail.com>
2024-11-10 23:43:44 -0500

    Before

--- YarbisIA/YarbisIA/urls.py
+++ YarbisIA/YarbisIA/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-import urls, mudandoIA
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include('mudandoIA.urls')),
+    path('', include('mudandoIA.urls')),
 ]
